b56a58608edba60a4aa32a3a7efc40dc5730cec5
Ruchira <ruchira@Ruchira.local>
2021-03-06 17:22:52 +0530

    Add Firebase function and Create reusable card

---
+++ functions/index.js
@@ -0,0 +1,49 @@
+import functions from "firebase-functions";
+import admin from "firebase-admin";
+// initializes your application
+
+admin.initializeApp(functions.config().firebase);
+
+// const functions = require("firebase-functions");
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+exports.sendPushNotification = functions.firestore
+  .document("some_collection/{some_document}")
+  .onCreate(event => {
+    // gets standard JavaScript object from the new write
+    const writeData = event.data.data();
+    // access data necessary for push notification 
+    const sender = writeData.uid;
+    const senderName = writeData.name;
+    const recipient = writeData.recipient;
+    // the payload is what will be delivered to the device(s)
+    let payload = {
+      notification: {
+      title: "Higer Bid Placed",
+      body: "Someone placed a higher bid that you have bidded",
+    //   sound:
+    //   badge:
+     }
+    }
+    // either store the recepient tokens in the document write
+    const tokens = writeData.tokens;  
+    
+    // or collect them by accessing your database
+    var pushToken = "";
+    return functions
+      .firestore
+      .collection("user_data_collection/recipient")
+      .get()
+      .then(doc => {
+         pushToken = doc.data().token;
+         // sendToDevice can also accept an array of push tokens
+         return admin.messaging().sendToDevice(pushToken, payload);
+      });
+});
---
+++ src/components/Card/Card.js
@@ -0,0 +1,184 @@
+import React from 'react'
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native'
+import NoPreViewImage from '../../assets/images/no_preview.png'
+
+
+export const Card = ({ item }) => (
+    <View>
+        <View style={styles.card}>
+            <View style={styles.cardDetails}>
+                <View style={styles.carouselContainer}>
+                    <Image style={styles.itemImage}
+                        source={{
+                            uri: 'https://reactnative.dev/img/tiny_logo.png',
+                        }}
+                    />
+                    <Image style={styles.itemImage}
+                        source={NoPreViewImage}
+                    />
+                </View>
+                <View style={styles.itemDetails}>
+                    <Text style={styles.itemTitle}>{item.title}</Text>
+                    <Text style={styles.itemDescription}>{item.body}</Text>
+                    <View style={styles.priceContainer}>
+                        <View style={styles.priceAmount}>
+                            <Text style={styles.baseprice}>100 $</Text>
+                            <Text style={styles.palceHolder}>Base Price</Text>
+                        </View>
+                        <View style={styles.priceAmount}>
+                            <Text style={styles.bidPrice}>150 $</Text>
+                            <Text style={styles.palceHolder}>Current Bid</Text>
+                        </View>
+                    </View>
+                </View>
+            </View>
+            <View style={styles.buttonAndTimer}>
+                <View style={styles.timerContainer}>
+                    <Text style={styles.timer}>20:00</Text>
+                    <Text style={styles.palceHolder}>Remaning Time</Text>
+                </View>
+                <TouchableOpacity style={styles.bidButton} onPress={() => bidNow(item)} >
+                    <Text style={styles.bidButtonText}>BID NOW</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    </View>
+);
+
+const styles = StyleSheet.create({
+    main: {
+        flex: 1,
+        alignItems: 'center'
+    },
+    card: {
+        backgroundColor: 'white',
+        width: 370,
+        marginTop: 10,
+        shadowColor: "#000",
+        elevation: 2,
+        borderRadius: 10,
+
+    },
+    cardDetails: {
+        flexDirection: 'row'
+    },
+    carouselContainer: {
+        position: 'relative',
+        top: 10,
+        width: 150,
+        height: 250,
+    },
+    slide1: {
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    activeDotStyle: {
+        width: 10,
+        height: 10,
+        borderRadius: 5,
+        backgroundColor: '#004E94'
+    },
+    dotStyle: {
+        width: 7,
+        height: 7,
+        borderRadius: 3.5,
+        backgroundColor: '#A5A5A7'
+    },
+    paginationStyle: {
+
+    },
+    itemImage: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        margin: 15,
+        width: 120,
+        height: 100
+    },
+    itemDetails: {
+        margin: 10
+    },
+    itemTitle: {
+        width: 200,
+        fontSize: 18,
+        fontWeight: 'bold'
+    },
+    itemDescription: {
+        marginTop: 5,
+        width: 200,
+        fontSize: 13,
+        fontWeight: '500'
+    },
+    timer: {
+        marginTop: 5,
+        fontSize: 12,
+        fontWeight: '100'
+    },
+    priceContainer: {
+        marginTop: 5,
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    priceAmount: {
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    baseprice: {
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+    bidPrice: {
+        fontSize: 22,
+        fontWeight: 'bold',
+    },
+    palceHolder: {
+        fontSize: 12,
+        fontWeight: 'normal',
+    },
+    buttonAndTimer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    },
+    timerContainer: {
+        width: 150,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    timer: {
+        fontSize: 15,
+        fontWeight: 'bold',
+        textAlign: 'center'
+    },
+    bidButton: {
+        width: 200,
+        height: 30,
+        backgroundColor: 'green',
+        alignSelf: 'flex-end',
+        margin: 10,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    bidButtonText: {
+        fontWeight: 'bold',
+        color: 'white'
+    },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        marginHorizontal: 16,
+    },
+    title: {
+        textAlign: 'center',
+        marginVertical: 8,
+        fontSize: 20,
+        fontWeight: 'bold'
+    },
+    separator: {
+        marginVertical: 8,
+        borderBottomColor: '#FFFFFF',
+        borderBottomWidth: StyleSheet.hairlineWidth,
+    },
+    textInput: {
+        backgroundColor: '#C0C0C0'
+    }
+});
