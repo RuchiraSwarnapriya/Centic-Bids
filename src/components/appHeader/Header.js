6b8d7e41e75e3fc922f402d66077883d6e769cbc
Ruchira <ruchira@Ruchira.local>
2021-03-16 09:17:22 +0530

    Add Header Change Back Icon Size

--- src/components/appHeader/Header.js
+++ src/components/appHeader/Header.js
@@ -1,15 +1,14 @@
 import React from 'react'
-import { Text, View, TouchableOpacity, Dimensions, StyleSheet } from 'react-native';
+import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
 import IonIcon from 'react-native-vector-icons/Ionicons';
 import { Colors } from "../../assets/colors";
 
-const window = Dimensions.get('window');
 
 const Header = ({navigation, title}) => { 
     return (
         <View style={styles.headerContainer}>
             <TouchableOpacity onPress={() => navigation.goBack()}>
-                <IonIcon name="chevron-back" size={(window.width) * 0.06} color={Colors.black} />
+                <IonIcon name="chevron-back" size={25} color={Colors.black} />
             </TouchableOpacity>
             <Text style={styles.title}>{title}</Text>
         </View>
@@ -19,9 +18,9 @@ const Header = ({navigation, title}) => {
 const styles = StyleSheet.create({
 
     headerContainer: {
-        marginLeft: (window.width) * 0.02,
-        marginTop: (window.width) * 0.02,
-        marginRight: (window.width) * 0.04,
+        marginLeft: 10,
+        marginTop: 10,
+        marginRight: 15,
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'space-between'
